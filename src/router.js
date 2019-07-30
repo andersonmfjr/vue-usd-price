e19a5f2a3d262c8fa329a5e3ae1ecbccb6fcddc5
Anderson Jr <andersonmfjr@gmail.com>
2019-07-29 23:00:04 -0300

    :rocket: Update page title dynamically with router meta fields

--- src/router.js
+++ src/router.js
@@ -1,39 +1,71 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import Buy from './views/Buy.vue';
 import Fees from './views/Fees.vue';
 import Exchanges from './views/Exchanges.vue';
 import Remittance from './views/Remittance.vue';
 import Compare from './views/Compare.vue';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Buy',
-      component: Buy,
+const routes = [
+  {
+    path: '/',
+    name: 'Buy',
+    component: Buy,
+    meta: {
+      title: 'Buy - USD Best Price',
     },
-    {
-      path: '/compare',
-      name: 'Compare',
-      component: Compare,
+  },
+  {
+    path: '/compare',
+    name: 'Compare',
+    component: Compare,
+    meta: {
+      title: 'Compare - USD Best Price',
     },
-    {
-      path: '/remittance',
-      name: 'Remittance',
-      component: Remittance,
+  },
+  {
+    path: '/remittance',
+    name: 'Remittance',
+    component: Remittance,
+    meta: {
+      title: 'Remittance - USD Best Price',
     },
-    {
-      path: '/exchanges',
-      name: 'Exchanges',
-      component: Exchanges,
+  },
+  {
+    path: '/exchanges',
+    name: 'Exchanges',
+    component: Exchanges,
+    meta: {
+      title: 'Exchanges - USD Best Price',
     },
-    {
-      path: '/fees',
-      name: "Exchanges' Fees",
-      component: Fees,
+  },
+  {
+    path: '/fees',
+    name: "Exchanges' Fees",
+    component: Fees,
+    meta: {
+      title: 'Fees - USD Best Price',
     },
-  ],
+  },
+];
+
+const router = new VueRouter({
+  routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find(r => r.meta && r.meta.title);
+
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title;
+  }
+
+  next();
+});
+
+export default router;
