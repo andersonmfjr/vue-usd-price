e4fe752a8dfb3f3a7468017f85441fef660dad1c
Anderson Jr <andersonmfjr@gmail.com>
2019-07-29 22:48:57 -0300

    :rocket: Update router for new pages

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Buy from './views/Buy.vue';
+import Fees from './views/Fees.vue';
+import Exchanges from './views/Exchanges.vue';
+import Remittance from './views/Remittance.vue';
+import Compare from './views/Compare.vue';
 
 Vue.use(Router);
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Buy',
+      component: Buy,
+    },
+    {
+      path: '/compare',
+      name: 'Compare',
+      component: Compare,
+    },
+    {
+      path: '/remittance',
+      name: 'Remittance',
+      component: Remittance,
+    },
+    {
+      path: '/exchanges',
+      name: 'Exchanges',
+      component: Exchanges,
+    },
+    {
+      path: '/fees',
+      name: "Exchanges' Fees",
+      component: Fees,
     },
   ],
 });
