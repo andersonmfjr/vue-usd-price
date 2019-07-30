944d2146f21dec925f8e0b91219cffcbbfbc44a5
Anderson Jr <andersonmfjr@gmail.com>
2019-07-29 23:09:59 -0300

    Add Roboto web font

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,19 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+html {
+  font-size: 10px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: 'Roboto', sans-serif;
+}
+
 #nav {
   padding: 30px;
   a {
